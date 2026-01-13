85cdb0de22bcd2d11b08be671d7b03bda84b3424
iceblinker <iceblinkluck@gmail.com>
2026-01-13 14:26:42 -0800

    Refactor: Cleanup legacy code, specialize for VixSrc, add Proxy config

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PYTHON_BIN=C:\Users\carlo\OneDrive\Documentos\GitHub\streamvix\.venv\Scripts\python.exe
-set PYTHONPATH=C:\Users\carlo\OneDrive\Documentos\GitHub\streamvix\.venv\Lib\site-packages
-npm start
